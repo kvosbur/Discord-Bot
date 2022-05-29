b0f2fd317dcc6caad35de8d30dfb52d826720303
Kevin <kevinvosburgh4@gmail.com>
2022-05-28 20:29:23 -0400

    Add logging

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 
 import discord
 from dotenv import load_dotenv
@@ -15,7 +16,10 @@ client = discord.Client()
 
 def start_client():
     global message_to_send
+    print(time.time())
     message_to_send = get_affix_data()
+    print("data retrieved")
+    print(message_to_send)
     client.run(token)
 
 
@@ -35,22 +39,28 @@ def get_channel(channels, target_name):
 @client.event
 async def on_ready():
     global target_channel
+    print("client readied")
     for guild in client.guilds:
         if guild.name == server:
             break
 
+    print(f"guild: {guild}")
     channel = get_channel(guild.channels, target_channel)
 
     # remove existing pins
     pinned_messages = await channel.pins()
     for m in pinned_messages:
         if m.author.id == client.user.id:
+            print(f"unpinning message: {m}")
             await m.unpin()
 
 
     # pin new message
     message = await channel.send(content=message_to_send)
+    print(f"sent message: {message}")
     await message.pin()
+    print("message pinned")
     await client.close()
+    print("client closed")
 
 start_client()
